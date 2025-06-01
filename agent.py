6fedc42ac4c53b4618f941eb698a79d28963c678
Tisan Das <TisanDas2011@gmail.com>
2025-06-01 20:41:45 +0530

    Added initial tools to the agent

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@
 #         return fixed_answer
     
 
-from smolagents import CodeAgent, DuckDuckGoSearchTool, OpenAIServerModel
+from smolagents import CodeAgent, tool, DuckDuckGoSearchTool, OpenAIServerModel
 import os
 
 # Initialize Gemini model using OpenAI-compatible API
@@ -22,5 +22,98 @@ model = OpenAIServerModel(
     api_key=os.GetEnv("KEY"),
 )
 
+
+from langchain_community.document_loaders import WikipediaLoader
+from langchain_community.document_loaders import ArxivLoader
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers.
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+@tool
+def add(a: int, b: int) -> int:
+    """Add two numbers.
+    
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+@tool
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers.
+    
+    Args:
+        a: first int
+        b: second int
+    """
+    return a - b
+
+@tool
+def divide(a: int, b: int) -> int:
+    """Divide two numbers.
+    
+    Args:
+        a: first int
+        b: second int
+    """
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+@tool
+def modulus(a: int, b: int) -> int:
+    """Get the modulus of two numbers.
+    
+    Args:
+        a: first int
+        b: second int
+    """
+    return a % b
+
+@tool
+def wiki_search(query: str) -> str:
+    """Search Wikipedia for a query and return maximum 2 results.
+    
+    Args:
+        query: The search query."""
+    search_docs = WikipediaLoader(query=query, load_max_docs=2).load()
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content}\n</Document>'
+            for doc in search_docs
+        ])
+    return {"wiki_results": formatted_search_docs}
+
+@tool
+def arvix_search(query: str) -> str:
+    """Search Arxiv for a query and return maximum 3 result.
+    
+    Args:
+        query: The search query."""
+    search_docs = ArxivLoader(query=query, load_max_docs=3).load()
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content[:1000]}\n</Document>'
+            for doc in search_docs
+        ])
+    return {"arvix_results": formatted_search_docs}
+
+
 # Create agent with Gemini model
-agent = CodeAgent(tools=[], model=model)
+agent = CodeAgent(tools=[
+    multiply,
+    add,
+    subtract,
+    divide,
+    modulus,
+    wiki_search,
+    DuckDuckGoSearchTool(),
+    arvix_search,
+], model=model)
